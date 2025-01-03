458c45f9f9d79527b294220a0ec2becddbffcfe9
Adytia855 <maditya855@gmail.com>
2025-01-03 21:35:26 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt aut placeat officiis?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
